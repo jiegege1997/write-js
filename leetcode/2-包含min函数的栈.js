ac6367973fc5c0dc9ccc3d5ccc454a4702fa591d
hjj <hujunjie0830@qq.com>
2022-03-23 23:23:29 +0800

    提交

---
+++ leetcode/2-包含min函数的栈.js
@@ -0,0 +1,40 @@
+// 不就是搞两个栈
+
+// 一个栈正常的  一个栈按最小的都放在上面
+
+
+ var MinStack = function() {
+  this.minStack = []
+  this.stack = []
+};
+
+/** 
+* @param {number} x
+* @return {void}
+*/
+MinStack.prototype.push = function(x) {
+  this.minStack.push(Math.min(this.min() ?? Infinity, x))
+  this.stack.push(x)
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function() {
+  this.minStack.pop()
+  this.stack.pop()
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1]
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.min = function() {
+  return this.minStack[this.minStack.length - 1]
+};
---
+++ leetcode/3-从尾到头打印链表.js
@@ -0,0 +1,12 @@
+
+
+// 思想其实就是栈  遍历一下 依次放进去即可
+
+var reversePrint = function(head) {
+  this.stack = []
+  while(head !== null) {
+      this.stack.unshift(head.val)
+      head = head.next
+  }
+  return this.stack
+};
