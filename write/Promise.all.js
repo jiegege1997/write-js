718a57e5df26d328ddb68967ea0014a6591101f7
hjj <hujunjie0830@qq.com>
2023-01-10 10:20:20 +0800

    格式

--- write/Promise.all.js
+++ write/Promise.all.js
@@ -1,29 +1,30 @@
 function myPromiseAll(arr) {
-  return new Promise((res,rej) => {
+  return new Promise((res, rej) => {
     let length = arr.length
     let fullfilledCount = 0
     let result = []
 
-    for(let i = 0; i < length; i++) {
-      arr[i].then(resolve => {
-        result.push(resolve)
-        fullfilledCount ++
-        if(fullfilledCount === length) {
-          res(result)
-        }
-      }).catch(error => {
-        rej(error)
-      })    
+    for (let i = 0; i < length; i++) {
+      arr[i]
+        .then((resolve) => {
+          result.push(resolve)
+          fullfilledCount++
+          if (fullfilledCount === length) {
+            res(result)
+          }
+        })
+        .catch((error) => {
+          rej(error)
+        })
     }
-
   })
 }
-let p1 = new Promise((res,rej) => {
-  setTimeout(()=>{
+let p1 = new Promise((res, rej) => {
+  setTimeout(() => {
     res("p1调用成功")
-  },500)
+  }, 500)
 })
-let p2 = new Promise((res,rej) => {
+let p2 = new Promise((res, rej) => {
   res("p2调用成功")
 })
 myPromiseAll([p1, p2]).then((res, rej) => {
